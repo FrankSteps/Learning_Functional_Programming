88f88c6708abc1cfdab85698300a0e5d556bef38
FrankSteps <franciscopassos.contato@gmail.com>
2025-10-20 21:45:26 -0300

    007 resolved

--- challenges/toExam/007_vinhos.hs
+++ challenges/toExam/007_vinhos.hs
@@ -3,10 +3,18 @@ di'vinho'dade
 
 Um supermercado está com oferta na compra de vinhos. Na compra de três garrafas paga-se apenas duas. Defina uma função que receba a quantidade de garrafas de vinho compradas e o preço individual de cada garrafa, sem descontos, e calcule o valor total a pagar.
 
-definindo os preços:
-    cada garrafa de vinho custa: 5 Reais
+
+- div quant de garrafas 3
+
 -}
 
+main :: IO()
+main = do
+    let quant = 200
+    let preco = 100
+    putStrLn("Resultado: " ++ show (diVinhoDade quant preco))
 
+diVinhoDade :: Int -> Float -> Float
+diVinhoDade quant preco = fromIntegral (quant - (div quant 3)) * preco
 
 
