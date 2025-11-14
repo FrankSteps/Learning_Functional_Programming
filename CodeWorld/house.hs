d4d512b1086be38db454e7514b836af64f47f260
Francisco Passos <franciscopassos.contato@gmail.com>
2025-11-13 21:55:59 -0300

    Refactor house drawing with new window and door details

--- CodeWorld/house.hs
+++ CodeWorld/house.hs
@@ -7,14 +7,21 @@ import CodeWorld
 
 -- função principal responsável por executar as demais funções
 main :: IO()
-main = drawingOf(
+main = drawingOf(-- coordinatePlane &
                  parede & telhado & 
                  translated (-1) (-0.5) porta & 
                  translated 1.5 0 janela &
                  chamine & 
                  translated (-0.3) (-0.5) trinco &
-                 translated 1.5 0 detalhe1 &
-                 translated 1.5 0 detalhe2)
+                 translated 1.5 0 janeladet1 &
+                 translated 1.5 0 janeladet2 &
+                 translated 1.5 (-1.05) janeladet3 &
+                 translated 1.5 0.025 janeladet4 &
+                 translated 1.5 5.1 chaminedet &
+                 translated 0 (-2.05) piso &
+                 translated (-1) (-0.475) portadet1 &
+                 translated (-0.3) (-0.62) portadet2 &
+                 translated (-0.3) (-0.83) abertura)
 
 --  funções responsáveis por desenhar cada parte da casa
 parede :: Picture 
@@ -33,13 +40,36 @@ chamine :: Picture
 chamine = polygon [(1, 3.67), (2, 2.83), (2, 5), (1, 5)]
 
 trinco :: Picture
-trinco = solidCircle 0.2 
+trinco = circle 0.15 
 
-detalhe1 :: Picture 
-detalhe1 = rectangle 0 2
+janeladet1 :: Picture 
+janeladet1 = rectangle 0 2
 
-detalhe2 :: Picture 
-detalhe2 = rectangle 2 0
+janeladet2 :: Picture 
+janeladet2 = rectangle 2 0
+
+janeladet3 :: Picture
+janeladet3 = rectangle 2.3 0.1
+
+janeladet4 :: Picture
+janeladet4 = rectangle 2.1 2.05
+
+chaminedet :: Picture 
+chaminedet = rectangle 1.3 0.2
+
+portadet1 :: Picture 
+portadet1 = rectangle 2.1 3.05
+
+portadet2 :: Picture
+portadet2 = rectangle 0.45 0.7
+
+abertura :: Picture 
+abertura = rectangle 0.07 0.14
+
+piso :: Picture 
+piso = rectangle 6 0.1
+
+
     
 
 
