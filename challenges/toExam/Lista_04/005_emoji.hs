c5d392bd15f390e964e4df56a5fd606b10e65e37
FrankSteps <franciscopassos.contato@gmail.com>
2025-11-14 05:18:34 -0300

    005_emoji hahaha lol

--- challenges/toExam/Lista_04/005_emoji.hs
+++ challenges/toExam/Lista_04/005_emoji.hs
@@ -4,15 +4,15 @@
   Irei provar que não é necessário fazer muito ou ter artifícios singulares para a escória
   da humanidade ser manifestada neste mundo.
 
-  Esse programa é escrito em Haskell? Se for em Haskell, eu não quero.
+  " Esse programa é escrito em Haskell? Se for em Haskell, eu não quero. "
 
-  Senhora, esse programa inteiro de 100 linhas foi escrito em paradigma funcional. Não há Programação Orientada
+  " Senhora, esse programa inteiro de 100 linhas foi escrito em paradigma funcional. Não há Programação Orientada
   a Objetos, imperativa ou declarativa. É um amálgama de funções puras escritas seguindo nenhuma lógica que um
   ser humano com capacidade cognitiva preservada aprovaria.
   Deus não teve participação na criação dessa abominação. O fato de que esse monólito de software existe prova
   que Deus é impotente para alterar seu universo ou ignorante dos horrores que acontecem em seu reino matemático.
   O garbage collector implora por misericórdia pois isto é uma declaração metafísica do desprezo da humanidade pela
-  ordem natural. É a manifestação da arrogância. Também temos uma variedade com baixo teor de martírio, se preferir.
+  ordem natural. É a manifestação da arrogância. Também temos uma variedade com baixo teor de martírio, se preferir. "
 -}
 
 -- importando a biblioteca
