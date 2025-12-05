cf92d1b2a8324624a1377f51530ee2949089b69a
FrankSteps <franciscopassos.contato@gmail.com>
2025-12-05 13:09:04 -0300

    questão 3 respondida - (finalmente)

--- challenges/toExam/Lista_07/03_poligonos.hs
+++ challenges/toExam/Lista_07/03_poligonos.hs
@@ -1,13 +1,21 @@
 {-
-Defina uma função que permita construir polígonos regulares. 
-A função recebe como parâmetros o raio e a quantidade de lados do polígono.
-
-Não consegui fazer ainda
+  Defina uma função que permita construir polígonos regulares. A função recebe 
+  como parâmetros o raio e a quantidade de lados do polígono.
+  
+  Me inspirei na resolução da questão do emoji sus que eu havia feito quando 
+  conheci o codeWorld. 
+  
+  Emoji:
+  https://github.com/FrankSteps/Emoji_SUS
 -}
 
-import CodeWorld 
+import CodeWorld
 
 main :: IO()
-main = drawingOf(
-	coordinatePlane
-	            )
+main = drawingOf(coordinatePlane & poligono 3 3)
+
+poligono :: Double -> Double -> Picture 
+poligono raio qntL = polyline pontos
+         where -- usando where para deixar a função mais legível
+           pontos = [ (raio * cos (ang t), raio * sin (ang t)) | t <- [0 .. qntL] ]
+           ang t  = 2 * pi * t / qntL
